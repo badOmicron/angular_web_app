74c3048ac07e126113bd07435e110d1eace1a071
Orlando Ramos <bhit.omicron@gmail.com>
2016-10-17 23:38:15 -0500

    ALPHA-0.0.2

--- js/services.js
+++ js/services.js
@@ -1,18 +1,16 @@
 'use strict';
 /* Services */
-var services = angular.module('ngdemo.services', ['ngResource']);
-//get the JSON data using the REST API call
+var services = angular.module('ngdemo.services', [ 'ngResource' ]);
+// se obtiene el recurso JSON haciendo uso de una llamada a una API de tipo REST
 
-services.factory('UserTaskService', function($http){
-     return {   
-        getUserTaskData: function(url) {		
-            return $http.post(url).then(function(result) {	
-                    return result.data;
-          });
-        }
-		}; 
-		 
-	
+// Se crea el modulo de servicio para tareas de usuario.
+services.factory('UserTaskService', function($http) {
+	return {
+		getUserTaskData : function(url) {
+			return $http.post(url).then(function(result) {
+				return result.data;
+			});
+		}
+	};
+
 });
-
-
